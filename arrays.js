566d627f23d0f9f71747ada1e88892b86334319c
Hilary Sbei <hilsbei@gmail.com>
2017-12-14 19:29:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 }
 
 function removeElementFromEndOfArray(array, element) {
-  return array.slice(2);
+  return array.slice(-1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array, element) {
